33787778fd869f2aa58bc5ae1f64c269e74d6b41
Kuba <kuba.dutk@gmail.com>
2021-07-05 00:14:39 +0200

    ShopCartModal final layout

--- src/components/menu/ShopCartModal.js
+++ src/components/menu/ShopCartModal.js
@@ -8,6 +8,7 @@ import { ToggleButtonGroup } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { DropdownButton } from 'react-bootstrap';
 import { Dropdown } from 'react-bootstrap';
+// import { MenuItem } from 'react-bootstrap';
 import { Container, Row, Col} from 'react-bootstrap';
 
 import '../Styles.css';
@@ -37,65 +38,71 @@ const ShopCartModal = (props) => {
         <Modal.Body>
           <Container>
             <Row>
-              <table class="table table-sm table-borderless">
-                <tbody>
-                  {props.dishes.filter(dish => dish.amount > 0).map(dish => {
-                      return <tr>
-                        <td>{dish.amount}x</td>
-                        <td>{dish.name}</td>
-                        <td style={{textAlign:"right"}}>{(dish.amount * dish.price).toFixed(2)} zł</td>
-                      </tr>
-                    })}
-                  <tr style={{borderTop:"1px solid lightgray", fontWeight:"bold"}}>
-                    {/* <td colspan="2"></td> */}
-                    <td></td>
-                    <td>Total cost: </td>
-                    <td style={{textAlign:"right"}}>{props.totalCost.toFixed(2)} zł</td>
-                  </tr>
-                </tbody>
-              </table>
-            </Row>
-
-            <br />
-            
-            <Row>
-              <Col sm={4}>
-                <p>Delivery addres:</p>
-              </Col>
-              <Col sm={8}>
-                <Button>{props.address}</Button>
+              <Col>
+                <table class="table table-sm table-borderless">
+                  <tbody>
+                    {props.dishes.filter(dish => dish.amount > 0).map(dish => {
+                        return <tr>
+                          <td>{dish.amount}x</td>
+                          <td>{dish.name}</td>
+                          <td style={{textAlign:"right"}}>{(dish.amount * dish.price).toFixed(2)} zł</td>
+                        </tr>
+                      })}
+                    <tr style={{borderTop:"1px solid lightgray", fontWeight:"bold"}}>
+                      {/* <td colspan="2"></td> */}
+                      <td></td>
+                      <td>Total cost: </td>
+                      <td style={{textAlign:"right"}}>{props.totalCost.toFixed(2)} zł</td>
+                    </tr>
+                  </tbody>
+                </table>
               </Col>
             </Row>
 
-            <Row>
+            <Row className="my-1">
               <Col sm={4}>
-                <p>Delivery time:</p>
+                {/* <span style={{fontSize:"14px"}}>Delivery addres:</span> */}
+                Delivery address:
               </Col>
               <Col sm={8}>
-               <DropdownButton id="dropdown-basic-button" className="d-inline-block" title="Dropdown button">
-                  <Dropdown.Item href="#/action-1">As soon as possible</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="#/action-2">1:00</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">1:30</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">2:00</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">2:30</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">3:00</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">3:30</Dropdown.Item>
-                </DropdownButton>
+                <Button block variant="warning" size="sm">{props.address}</Button>
               </Col>
             </Row>
 
-            <Row>
+            <Row className="my-1">
               <Col sm={4}>
-                <p>Payment method:</p>
+                Delivery time:
               </Col>
               <Col sm={8}>
-              <ButtonGroup toggle>
+                <Dropdown block>
+                  <Dropdown.Toggle block size="sm">
+                    As soon as possible
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">As soon as possible</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-2">1:00</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">1:30</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">2:00</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">2:30</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3:00</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3:30</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
+            </Row>
+
+            <Row className="my-1">
+              <Col sm={4}>
+                Payment method:
+              </Col>
+              <Col sm={8}>
+              <ButtonGroup toggle className="btn-block" size="sm">
                   {paymentMethods.map((radio, idx) => (
                     <ToggleButton
                       key={idx}
                       type="radio"
-                      variant={radio.value === paymentMethodRadioValue ? "primary" : "secondary"}
+                      variant="outline-primary"
                       name="radio"
                       value={radio.value}
                       checked={paymentMethodRadioValue === radio.value}
@@ -109,12 +116,16 @@ const ShopCartModal = (props) => {
             </Row>
 
             <Row>
-            <Form.Control placeholder="Order notes" />
+              <Col>
+                <Form.Control placeholder="Order notes"/>
+              </Col>
             </Row>
+
           </Container>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={props.onHide}>Make order</Button>
+          <Button onClick={props.onHide} variant="light">Back to menu</Button>
+          <Button onClick={props.onHide} variant="success">Make order</Button>
         </Modal.Footer>
       </Modal>
     );
